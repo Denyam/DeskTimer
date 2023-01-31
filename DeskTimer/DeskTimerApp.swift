51fc76ba40d5ff476cbc8b1d5bb530e141e9f4ab
Denis <denis.melenevsky@gmail.com>
2023-01-31 20:33:12 +0200

    Initial Commit

---
+++ DeskTimer/DeskTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeskTimerApp.swift
+//  DeskTimer
+//
+//  Created by Denis on 31.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct DeskTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
